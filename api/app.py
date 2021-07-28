49c210c4e2fbd142a512a8dc2e486cd1b7472431
javier vela <36648910+javier00vela@users.noreply.github.com>
2021-07-27 19:37:11 -0500

    Update app.py

--- api/app.py
+++ api/app.py
@@ -9,4 +9,4 @@ cors = CORS(app , support_credentials=True)
 loader_routes(app)
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0' , debug = True)
+    app.run(host = '0.0.0.0' , debug = False)
